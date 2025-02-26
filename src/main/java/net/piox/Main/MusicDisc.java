e641317a9124fb3e0695c22d71031426ac9c3abe
Piotr <pioxpl12@gmail.com>
2025-02-26 23:18:10 +0100

    Start Project

---
+++ src/main/java/net/piox/Main/MusicDisc.java
@@ -0,0 +1,15 @@
+package net.piox.Main;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MusicDisc implements ModInitializer {
+	public static final String MOD_ID = "musicdisc";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/piox/Main/MusicDiscClient.java
@@ -0,0 +1,11 @@
+package net.piox.Main;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MusicDiscClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
